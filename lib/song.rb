e9026e382f606376c36c4d4377c834bb47707d51
Hanna Lee <mani21na@gmail.com>
2019-09-28 07:05:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -41,7 +41,7 @@ class Song
     self.class.table_name
   end
 
-  # ?? I need digging it!!!
+  # ?? I need digging it!!!********************************
   def values_for_insert
     values = []
     self.class.column_names.each do |col_name|
